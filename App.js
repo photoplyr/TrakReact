0e57af2914ad51b5f6aa3216ee134e3f3a2ae6aa
Troy Simon <troy.simon@gmail.com>
2018-06-26 15:20:18 -0700

    Quick update

--- App.js
+++ App.js
@@ -27,11 +27,12 @@ export default class App extends React.Component {
     }
   }
 
+  // Load all the image reso
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
+        // require('./assets/images/robot-dev.png'),
+        // require('./assets/images/robot-prod.png'),
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
