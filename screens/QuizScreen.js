a709a115452b97e20f1150a3aeab40cb26bafcdf
Danil Fedorov <fedorov.danil@gmail.com>
2018-08-08 15:47:18 +0300

    Quiz embed test Token

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { WebView } from 'react-native';
 
+let userToken = AsyncStorage.getItem('userToken');
+
 export default class QuizScreen extends React.Component {
   static navigationOptions = {
     title: 'Links',
@@ -11,6 +13,7 @@ export default class QuizScreen extends React.Component {
     return (
       <WebView
         source={{uri: 'https://trakfertility.tools/api/embed/test/quiz/2'}}
+        injectedJavaScript={'(var Token = "'+ userToken +'");'}
       />
     );
   }
